c789eaa6b20c5975475e239a3d6bdc0df5574519
wasim7x <wasim7x@gmail.com>
2025-05-09 19:30:52 +0530

    intialige data_ingestion

---
+++ data_ingestion/data_ingest.py
@@ -0,0 +1,14 @@
+import pandas as pd
+# import langchain_astradb import AstraDBVectorStore
+from dotenv import load_dotenv
+import os
+from data_ingestion.data_transform import data_conveter
+    
+class ingest_data:
+    def __init__(self):
+         print("data ingestion intialize..")
+    def data_ingestion(self):
+         pass
+    
+if __name__== '__main__':
+    data_ingestion=ingest_data()    
--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -1,1 +1,14 @@
 import pandas as pd
+from langchain_core.documents import Document
+
+
+class data_conveter:
+    def __init__(self):
+        
+        pass
+
+    def data_transformation(self):
+        pass
+
+    # if __name__ == 'main':
+    #     data_ingestion = data_ingestion(     )
